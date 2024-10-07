86765e552d36b47c592e80f5d11f460bd72860b6
erickchff@gmail.com <erickchff@gmail.com>
2024-10-07 11:07:46 -0300

    primeiro projeto java

---
+++ 1st project/src/executavelJava/FirstTest.java
@@ -0,0 +1,9 @@
+package executavelJava;
+
+public class FirstTest {
+	
+	/*apertando control depois espaço para selecionar o código mais rápido*/
+    public static void main(String[] args) {
+		System.out.println("HELLO WORLD!!");
+	}
+}
