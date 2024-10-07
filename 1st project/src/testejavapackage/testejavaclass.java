46e41a485dc7dd147ecfa4dbb20145c65df7b2cd
erickchff@gmail.com <erickchff@gmail.com>
2024-10-07 18:59:38 -0300

    update

--- 1st project/src/testejavapackage/testejavaclass.java
+++ 1st project/src/testejavapackage/testejavaclass.java
@@ -1,5 +1,24 @@
 package testejavapackage;
 
 public class testejavaclass {
+	
+	private String idade;
+	
+	private String genero;
 
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 }
